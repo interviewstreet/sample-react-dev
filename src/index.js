cb6bc3185bc6df85fb1eb5dc130b355ae129a8b1
Ameer Jhan <ameerjhanprof@gmail.com>
2022-05-29 15:02:00 +0530

    feat: more change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, { Hackerrank } from './App';
+import { Button } from './components/button';
+
+console.log(Hackerrank, Button);
 
 ReactDOM.render(<App />, document.getElementById('root'));
